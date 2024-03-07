82bc5027effc6baabd661c93153601e57cc3aaad
LudmilaAzevedo <161759503+LudmilaAzevedo@users.noreply.github.com>
2024-03-07 15:45:59 -0300

    Add files via upload

---
+++ atividade2.ts
@@ -0,0 +1,21 @@
+/*
+Crie um programa que peça um número ao usuário e armazene ele na variável x. 
+Depois peça outro número e armazene na variável y.Mostre esses números. 
+Em seguida, faça com que x passe a ter o valor de y, e que y passe a ter o valor de x.
+Nome: Ludmila
+*/
+
+import { Z_ASCII } from "zlib";
+
+let teclado = require (`prompt-sync`)();
+let x: number = parseFloat(teclado(`Digite o valor de x: `));
+let y: number = parseFloat(teclado(`Digite o valor de y: `));
+let z: number = 0;
+
+console.log(`O valor de x é ${x} o valor de y é ${y}`);
+
+z = x;
+x = y;
+y = z;
+
+console.log(`O valor de y é ${x} e o valor de x é ${z}`);
---
+++ atividade3.ts
@@ -0,0 +1,14 @@
+/*
+Faça um programa que verifique se uma letra digitada é vogal ou consoante.  
+Nome: Ludmila
+*/
+
+let teclado = require (`prompt-sync`)();
+let letra = teclado(`Digite uma letra: `);
+if (letra == `A`|| letra == `a` || letra == `E` || letra == `e` || letra == `I` || letra == `i` || letra == `O` 
+|| letra == `o` || letra == `U` || letra == `u`) {
+    console.log(`A letra digitada "${letra}" é uma vogal`);
+}
+else {
+    console.log(`A letra digitada "${letra}" é uma consoante`);
+}
